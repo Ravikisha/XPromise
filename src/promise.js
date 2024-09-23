e576af55c315582ca9377e9baa8de03cbe98ac37
ravikishan.69 <ravikishan63392@gmail.com>
2024-09-23 17:30:59 +0530

    One-Way transition

--- src/promise.js
+++ src/promise.js
@@ -1,51 +1,56 @@
 // Possible states: pending, fulfilled, rejected
-const PENDING = 'PENDING';
-const FULFILLED = 'FULFILLED';
-const REJECTED = 'REJECTED';
+const PENDING = "PENDING";
+const FULFILLED = "FULFILLED";
+const REJECTED = "REJECTED";
 
 class RPromise {
-    constructor(executor) {
-        // Initial state is pending
-        this.state = PENDING;
-        // call the executor function immediately
-        doResolve(this, executor);
-    }
+  constructor(executor) {
+    // Initial state is pending
+    this.state = PENDING;
+    // call the executor function immediately
+    doResolve(this, executor);
+  }
 
-    // `then` method handles the fulfillment and rejection of a promise
-    then(onFulfilled, onRejected) {
-        handleResolved(this, onFulfilled, onRejected);
-    }
+  // `then` method handles the fulfillment and rejection of a promise
+  then(onFulfilled, onRejected) {
+    handleResolved(this, onFulfilled, onRejected);
+  }
 }
 
 // fulfill with `reason`
 function fulfill(promise, reason) {
-    promise.state = FULFILLED;
-    promise.value = reason;
+  promise.state = FULFILLED;
+  promise.value = reason;
 }
 
 // reject with `reason`
 function reject(promise, reason) {
-    promise.state = REJECTED;
-    promise.value = reason;
+  promise.state = REJECTED;
+  promise.value = reason;
 }
 
 // creates the fulfill and reject functions that are passed to the executor
 function doResolve(promise, executor) {
-    function wrapFulfill(value) {
-        fulfill(promise, value);
-    }
+  let called = false;
+  function wrapFulfill(value) {
+    if (called) return;
+    called = true;
+    fulfill(promise, value);
+  }
 
-    function wrapReject(reason) {
-        reject(promise, reason);
-    }
+  function wrapReject(reason) {
+    if (called) return;
+    called = true;
+    reject(promise, reason);
+  }
 
-    executor(wrapFulfill, wrapReject);
+  executor(wrapFulfill, wrapReject);
 }
 
 // handle the fulfillment and rejection of a promise
 function handleResolved(promise, onFulfilled, onRejected) {
-    const cb = promise.state === FULFILLED ? onFulfilled : onRejected;
-    cb(promise.value);
+  const cb = promise.state === FULFILLED ? onFulfilled : onRejected;
+  cb(promise.value);
 }
 
 module.exports = RPromise;
--- tests/promise.test.js
+++ tests/promise.test.js
@@ -64,3 +64,40 @@ describe("Observing state changes", () => {
     expect(onRejected.mock.calls[0][0]).toBe(reason);
   });
 });
+
+describe("One way transition", () => {
+  const value = ":)";
+  const reason = "I failed :(";
+
+  it("when a promise is fulfilled it should not be rejected with another value", () => {
+    const onFulfilled = jest.fn();
+    const onRejected = jest.fn();
+
+    const promise = new RPromise((resolve, reject) => {
+      resolve(value);
+      reject(reason);
+    });
+    promise.then(onFulfilled, onRejected);
+
+    expect(onFulfilled.mock.calls.length).toBe(1);
+    expect(onFulfilled.mock.calls[0][0]).toBe(value);
+    expect(onRejected.mock.calls.length).toBe(0);
+    expect(promise.state === "FULFILLED");
+  });
+
+  it("when a promise is rejected it should not be fulfilled with another value", () => {
+    const onFulfilled = jest.fn();
+    const onRejected = jest.fn();
+
+    const promise = new RPromise((resolve, reject) => {
+      reject(reason);
+      resolve(value);
+    });
+    promise.then(onFulfilled, onRejected);
+
+    expect(onRejected.mock.calls.length).toBe(1);
+    expect(onRejected.mock.calls[0][0]).toBe(reason);
+    expect(onFulfilled.mock.calls.length).toBe(0);
+    expect(promise.state === "REJECTED");
+  });
+});
